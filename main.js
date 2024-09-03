d189ba20cef4f600115107c864a9b1041a930492
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2024-09-04 00:28:09 +0800

    import bootstrap

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1600,
     height: 1200,
+    
   });
 
   win.loadFile("dist/index.html");
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import "../src/scss/style.scss"
+
 document.body.innerHTML = '<div id="app"></div>';
 
 const root = createRoot(document.getElementById("app"));
@@ -8,5 +10,10 @@ const root = createRoot(document.getElementById("app"));
 root.render(<App />);
 
 function App(){
-  return <h1>MyMood</h1>
+  return(<>
+  <div id="winCtrl-bar">
+
+  </div>
+  <button className="btn btn-primary">test</button>
+  </>)
 }
