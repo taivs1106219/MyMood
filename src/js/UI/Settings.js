10b83d207c47ce1c2f1c3a88b36991219c29c9f8
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2024-09-11 18:17:07 +0800

    add color picker

--- src/js/UI/Settings.js
+++ src/js/UI/Settings.js
@@ -3,6 +3,26 @@ import * as cn from "classnames";
 
 import MenuButton from "./MenuButton";
 
+const bg_colors = [
+  "#ffffff",
+  "#ffcccc",
+  "#ffffcc",
+  "#ccffcc",
+  "#ccffff",
+  "#ccccff",
+  "#ffccff",
+];
+
+const front_colors = [
+  "#ffffff",
+  "#ff5050",
+  "#ffff00",
+  "#33cc33",
+  "#33cccc",
+  "#3366ff",
+  "#ff00ff",
+];
+
 function Settings() {
   return (
     <>
@@ -11,17 +31,53 @@ function Settings() {
         <h2>設定</h2>
       </div>
       <div>
-        <div class="input-group flex-nowrap">
-          <span class="input-group-text" id="addon-wrapping">
-            昵稱
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Username"
-            aria-label="Username"
-            aria-describedby="addon-wrapping"
-          ></input>
+        <div className="container">
+          <div class="input-group flex-nowrap mb-3">
+            <span class="input-group-text" id="addon-wrapping">
+              暱稱
+            </span>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Username"
+              aria-label="Username"
+              aria-describedby="addon-wrapping"
+            ></input>
+          </div>
+          <div className="w-100">
+            <div className="input-group d-flex mb-3" role="group">
+              <span className="input-group-text">背景顏色</span>
+              {bg_colors.map((e) => {
+                return (
+                  <input
+                    className="btn btn-outline-secondary flex-fill"
+                    type="button"
+                    style={{ backgroundColor: e }}
+                    key={e}
+                    value={" "}
+                  ></input>
+                );
+              })}
+            </div>
+            <div className="input-group d-flex" role="group">
+              <span className="input-group-text">強調色</span>
+              {front_colors.map((e) => {
+                return (
+                  <input
+                    className={
+                      e == "#ffffff"
+                        ? "btn btn-outline-secondary"
+                        : "btn btn-outline-secondary flex-fill"
+                    }
+                    type="button"
+                    style={{ backgroundColor: e }}
+                    key={e}
+                    value={e == "#ffffff" ? "恢復預設" : " "}
+                  ></input>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </>
