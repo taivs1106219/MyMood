3d6cef9547cc285e21da94f1b51ac7038371f272
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2024-09-13 20:46:18 +0800

    change app window size

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let config;
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 480,
-    height: 720,
+    width: 600,
+    height: 800,
     frame: false,
     webPreferences: {
       preload: path.join(__dirname, "./preload.js"),
