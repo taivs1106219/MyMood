9ca999cc56d7b6174d12200821dbf4fee8ff3b7c
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2024-09-25 19:15:39 +0800

    add images and buttons

--- src/js/UI/MyPet.js
+++ src/js/UI/MyPet.js
@@ -1,16 +1,34 @@
 import React from "react";
 import MenuButton from "./MenuButton";
+import zanghu from "../../../res/images/zanghu.png";
 
-function MyPet() {
+function MyPet(e) {
+  function handlePetClick() {
+    console.log("pet clicked");
+  }
   return (
     <>
-      <MenuButton></MenuButton>
-      <div className="container">
-        <div className="w-100 row justify-content-center">
-          <button className="display-1 text-center">寵物</button>
-        </div>
-        <div className="row">
+      <div className="" id="main-content" style={{ overflowX: "hidden" }}>
+        <MenuButton></MenuButton>
 
+        <div className="container">
+          <div
+            className="w-100 row justify-content-center"
+            onClick={(e) => handlePetClick(e)}
+          >
+            <img className="w-100" src={zanghu}></img>
+          </div>
+          <div className="row row-cols-3 w-100">
+            <div className="col justify-content-center text-center">
+              <button className="btn btn-info">加水</button>
+            </div>
+            <div className="col justify-content-center text-center">
+              <button className="btn btn-info">餵食</button>
+            </div>
+            <div className="col justify-content-center text-center">
+              <button className="btn btn-info">開發中</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
