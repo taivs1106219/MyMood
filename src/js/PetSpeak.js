24d1574876aee41dcd4bba0a41d0cc850c315104
taivs1106219 <68107903+ryantsui1109@users.noreply.github.com>
2024-10-01 15:48:34 +0800

    move petspeak file location

--- src/js/PetSpeak.js
+++ src/js/PetSpeak.js
@@ -2,3 +2,7 @@ function touch() {
   const says = ["呼嚕呼嚕～", "叮叮叮叮"];
   return says[Math.floor(Math.random() * (says.length + 1))];
 }
+
+const petSpeak = { touch: touch() };
+
+export default petSpeak;
